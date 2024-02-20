b098f30ff1c386532957ecc2c7f417ba2ce6aabb
Zea-Zee <g.kuznetsov2@g.nsu.ru>
2024-02-20 17:57:44 +0700

    start parser

---
+++ networks/parser.py
@@ -0,0 +1,24 @@
+from requests import Request, Session
+from requests.exceptions import ConnectionError, Timeout, TooManyRedirects
+import json
+
+url = 'https://sandbox-api.coinmarketcap.com/v1/cryptocurrency/listings/latest'
+parameters = {
+    'start': '1',
+    'limit': '5000',
+    'convert': 'USD'
+}
+headers = {
+    'Accepts': 'application/json',
+    'X-CMC_PRO_API_KEY': '7cb7dce9-a6ef-4386-897c-f8887d2855a9',
+}
+
+session = Session()
+session.headers.update(headers)
+
+try:
+    response = session.get(url, params=parameters)
+    data = json.loads(response.text)
+    print(data)
+except (ConnectionError, Timeout, TooManyRedirects) as e:
+    print(e)
