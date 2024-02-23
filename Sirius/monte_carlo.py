51c0571097a9da00b7527bc7a6ae56bf0b11a897
Zea-Zee <g.kuznetsov2@g.nsu.ru>
2024-02-23 16:33:40 +0700

    RL

---
+++ Sirius/monte_carlo.py
@@ -0,0 +1,22 @@
+# подключаем модуль случайных чисел
+import random
+import time
+
+
+def count_pi(n):
+    start = time.time()
+    i = 0
+    count = 0
+    while i < n:
+        x = random.random()
+        y = random.random()
+        if (pow(x, 2) + pow(y, 2)) < 1:
+            count += 1
+        i += 1
+    print(f"Estimated time: {time.time() - start} s")
+    return 4 * (count / n)
+
+print(count_pi(10000000))
+print(count_pi(100000000))
+print(count_pi(1000000000))
+print(count_pi(1000000000))
