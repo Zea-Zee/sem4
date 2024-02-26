fe25f79248cee3b475428774b507eb1df59bef5c
Zea-Zee <glebru.irk38@yandex.ru>
2024-02-26 19:43:46 +0700

    start lab_4 PHC

--- Parallelism/2ndLab/integral.c
+++ Parallelism/2ndLab/integral.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <math.h>
 #include <time.h>
 #include <omp.h>
+#include <string.h>
 
 
 const double PI = 3.14159265358979323846;
@@ -9,33 +11,26 @@ const double a = -4.0;
 const double b = 4.0;
 const int nsteps = 40000000;
 
-double cpuSecond()
-{
+double cpuSecond() {
     struct timespec ts;
     timespec_get(&ts, TIME_UTC);
     return ((double)ts.tv_sec + (double)ts.tv_nsec * 1.e-9);
 }
 
-double func(double x)
-{
+double func(double x) {
     return exp(-x * x);
 }
 
-double integrate(double (*func)(double), double a, double b, int n)
-{
+double integrate(double (*func)(double), double a, double b, int n) {
     double h = (b - a) / n;
     double sum = 0.0;
-
     for (int i = 0; i < n; i++)
         sum += func(a + h * (i + 0.5));
-
     sum *= h;
-
     return sum;
 }
 
-double integrate_omp(double (*func)(double), double a, double b, int n)
-{
+double integrate_omp(double (*func)(double), double a, double b, int n) {
     double h = (b - a) / n;
     double sum = 0.0;
 #pragma omp parallel
@@ -55,36 +50,47 @@ double integrate_omp(double (*func)(double), double a, double b, int n)
     return sum;
 }
 
-double run_serial()
-{
+double run_serial() {
     double t = cpuSecond();
     double res = 0;
-    for(int i = 0; i < 10; i++)
+    for (int i = 0; i < 50; i++)
         res += integrate(func, a, b, nsteps);
-    t = (cpuSecond() - t) / 10;
-    res /= 10;
+    t = (cpuSecond() - t) / 50;
+    res /= 50;
     printf("Result (serial): %.12f; error %.12f\n", res, fabs(res - sqrt(PI)));
     return t;
 }
-double run_parallel()
-{
+
+double run_parallel() {
     double t = cpuSecond();
     double res = 0;
-    for(int i = 0; i < 10; i++)
+    for (int i = 0; i < 10; i++)
         res += integrate_omp(func, a, b, nsteps);
     t = (cpuSecond() - t) / 10;
     res /= 10;
     printf("Result (parallel): %.12f; error %.12f\n", res, fabs(res - sqrt(PI)));
     return t;
 }
-int main(int argc, char **argv)
-{
-    printf("Integration f(x) on [%.12f, %.12f], nsteps = %d\n", a, b, nsteps);
-    double tserial = run_serial();
-    double tparallel = run_parallel();
 
-    printf("Execution time (serial): %.6f\n", tserial);
-    printf("Execution time (parallel): %.6f\n", tparallel);
-    printf("Speedup: %.2f\n", (double) 0.7182 / tparallel);
+int main(int argc, char **argv) {
+    // printf("Integration f(x) on [%.12f, %.12f], nsteps = %d\n", a, b, nsteps);
+    double tserial = 0;
+    if (argc == 1 || (argc > 1 && strcmp(argv[1], "-parallel") != 0)){
+        tserial = run_serial();
+        FILE *file = fopen("serial_integrate_time.txt", "w");
+        fprintf(file, "%.6f", tserial);
+        fclose(file);
+    }
+
+
+    if (argc > 1 && strcmp(argv[1], "-parallel") == 0){
+        double tparallel = run_parallel();
+
+        FILE *file = fopen("serial_integrate_time.txt", "r");
+        double serialTime;
+        fscanf(file, "%lf", &serialTime);
+        printf("Speedup: %.2f\n", serialTime / tparallel);
+    }
+
     return 0;
 }
--- Parallelism/2ndLab/lab_2.2_launcher.py
+++ Parallelism/2ndLab/lab_2.2_launcher.py
@@ -1,15 +1,47 @@
 import subprocess
 import pandas as pd
+import matplotlib.pyplot as plt
 import re
+# import seaborn as sns
 
 
-cpu_flags = ['1-2', '1-4']#, '1-8', '12', '1-16',
-             #'1-20', '1-24', '1-28', '1-32', '1-36', '1-40', '1-80']
+def build_plot(df):
+    indices = [int(col) for col in df.columns]
+    values = [float(val) for val in df.iloc[0].values]
+
+    plt.figure(figsize=(10, 10))
+    plt.title("Speedup dependency from threads")
+
+    plt.plot(indices, values, marker='o', label='Speedup')
+    plt.plot(indices, indices, linestyle='--', color='red', label='y=x')
+
+    plt.xticks(indices)
+    plt.yticks(values)
+
+    plt.xlabel('T num')
+    plt.ylabel('Speedup')
+
+    plt.grid(True)
+    plt.legend()
+    plt.savefig("lab2_2.png")
+    plt.close()
 
 
-data = []
+cpu_flags = ['1-2', '1-4', '1-8', '1-12', '1-16', '1-20',
+             '1-24', '1-28', '1-32', '1-36', '1-40', '1-44', '1-48', '1-52', '1-56', '1-60', '1-64', '1-68', '1-72', '1-76', '1-80']
+
+# cpu_flags = ['1-2', '1-32', '1-36', '1-40', '1-60', '1-80']
+# cpu_flags = ['1-2', '1-4', '1-8', '1-12']
+# cpu_flags = ['1-8', '1-12']
+
+
+subprocess.run("./integral", shell=True, capture_output=True, text=True)
+
+
+data = {}
+data['1'] = [1]
 for flag in cpu_flags:
-    command = f"taskset -c {flag} ./integral"
+    command = f"taskset -c {flag} ./integral -parallel"
     result = subprocess.run(command, shell=True,
                             capture_output=True, text=True)
 
@@ -17,15 +49,9 @@ for flag in cpu_flags:
     last_line = output_lines[-1]
     match = re.search(
         r'Speedup: (\d+\.\d+)', last_line)
-    if match:
-        time_taken = match.group(1)
-    else:
-        time_taken = None
-    print(time_taken)
-# data.append(row_data)
+    speedup = match.group(1)
+    data[int(flag.split('-')[1])] = [float(speedup)]
+print(data)
 
-
-# results_df = pd.DataFrame(data)
-# results_df.set_index('Size', inplace=True)
-# results_df.to_csv('results.csv')
-# print("Результаты успешно записаны в файл results.csv")
+results_df = pd.DataFrame(data)
+build_plot(results_df)
